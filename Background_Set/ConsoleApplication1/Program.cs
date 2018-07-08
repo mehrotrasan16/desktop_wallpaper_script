e95787fba4464cfecd99d07d695bfc9d7b7ad581
Sanket Mehrotra <sanketmehrotra101@gmail.com>
2018-07-08 22:22:17 +0530

    C# script to set your desktop wallpaper.
    completed part 1 of the software structure.

---
+++ Background_Set/ConsoleApplication1/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+//using Newtonsoft.Json;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+
+
+
+namespace Background
+{
+    //extended class to remember the filename/path as well as the image datatype in memory.
+    class ImageExt
+    {
+        public Image Image { get; set; }
+        public String Filename { get; set; }
+
+        public ImageExt(string Filename)
+        {
+            this.Image = Image.FromFile(Filename);
+            this.Filename = Filename;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageExt Background = new ImageExt(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\Wallpapers\\HigennoAce-1.jpg");//Image.FromFile(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "\\Wallpapers\\HigennoAce-1.jpg");
+            SetBackground(Background, PicturePosition.Fill);
+        }
+
+        private static void SetBackground(ImageExt background, PicturePosition style)
+        {
+            Console.WriteLine("Setting Wallpaper");
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            //PrintUserKeys(key);
+            switch (style)
+            {
+                case PicturePosition.Tile:
+                    key.SetValue(@"PicturePosition", "0");
+                    key.SetValue(@"TileWallpaper", "1");
+                    break;
+                case PicturePosition.Center:
+                    key.SetValue(@"PicturePosition", "0");
+                    key.SetValue(@"TileWallpaper", "0");
+                    break;
+                case PicturePosition.Stretch:
+                    key.SetValue(@"PicturePosition", "2");
+                    key.SetValue(@"TileWallpaper", "0");
+                    break;
+                case PicturePosition.Fit:
+                    key.SetValue(@"PicturePosition", "6");
+                    key.SetValue(@"TileWallpaper", "0");
+                    break;
+                case PicturePosition.Fill:
+                    key.SetValue(@"PicturePosition", "10");
+                    key.SetValue(@"TileWallpaper", "0");
+                    break;
+            }
+            key.Close();
+
+            const int SET_DESKTOP_BACKGROUND = 20;
+            const int UPDATE_INI_FILE = 1;
+            const int SEND_WINDOWS_INI_CHANGE = 2;
+            NativeMethods.SystemParametersInfo(SET_DESKTOP_BACKGROUND, 0, getBackgroundPath(background), UPDATE_INI_FILE | SEND_WINDOWS_INI_CHANGE);
+
+            Console.WriteLine("Wallpaper set to Image at path ");
+
+            Console.ReadKey();
+        }
+
+        private static string getBackgroundPath(ImageExt background)
+        {
+            return background.Filename;
+        }
+
+        static void PrintUserKeys(RegistryKey key)
+        {
+            String[] names = key.GetSubKeyNames();
+
+            int icount = 0;
+
+            const string userRoot = "HKEY_CURRENT_USER";
+
+            Console.WriteLine("subkeys for current user are ");
+            Console.WriteLine("------------------------------");
+
+            foreach (String s in names)
+            {
+                Console.WriteLine(s + Registry.CurrentUser.GetValue(s,"\tNone"));
+                //TODO: figure out how to print all user subkey values
+                icount++;
+                if(icount > 10)
+                {
+                    break;
+                }
+            }
+        }
+
+        public enum PicturePosition
+        {
+            Tile,
+            Center,
+            Fill,
+            Stretch,
+            Fit
+        }
+
+        internal sealed class NativeMethods
+        {
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            internal static extern int SystemParametersInfo(
+                int uAction,
+                int uParam,
+                String lpvParam,
+                int fuWinIni
+                );
+        }
+    }
+}
---
+++ Background_Set/ConsoleApplication1/Ref_code_setting.cs
@@ -0,0 +1,96 @@
+﻿//From URL: https://codereview.stackexchange.com/questions/60755/set-desktop-background
+using System;
+using Newtonsoft.Json;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+
+
+namespace Background
+{
+    class Program_REF_CODE_SET_BCKGROUND
+    {
+        static void Main_setting(string[] args)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                Rectangle resolution = new Rectangle(0, 0, 1920, 1080);//Screen.PrimaryScreen.Bounds;
+                string json = webClient.DownloadString("super secret website url");
+                dynamic results = JsonConvert.DeserializeObject<dynamic>(json);
+                string url = "domain.com" + results.images[0].urlbase;
+                if (resolution.Width <= 1920 && resolution.Height <= 1200)
+                {
+                    url += String.Format("_{0}x{1}.jpg", resolution.Width, resolution.Height);
+                }
+                else
+                {
+                    url += "_1920x1200.jpg";
+                }
+                DesktopBackground desktopBackground = new DesktopBackground();
+                desktopBackground.Set(url, PicturePosition.Fill);
+            }
+        }
+    }
+    public enum PicturePosition
+    {
+        Tile, Center, Stretch, Fit, Fill
+    }
+    public class DesktopBackground
+    {
+        public DesktopBackground() { }
+        const int SET_DESKTOP_BACKGROUND = 20;
+        const int UPDATE_INI_FILE = 1;
+        const int SEND_WINDOWS_INI_CHANGE = 2;
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        public void Set(string URL, PicturePosition style)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(URL);
+            HttpWebResponse httpWebReponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            Stream stream = httpWebReponse.GetResponseStream();
+            Image backgroundImage = Image.FromStream(stream);
+            int backgroundNumber = 0;
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + "/Backgrounds/";
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            string fullBackgroundFilePath = Path.Combine(directory, String.Format("background{0}.bmp", backgroundNumber));
+            while (System.IO.File.Exists(fullBackgroundFilePath))
+            {
+                backgroundNumber++;
+                fullBackgroundFilePath = Path.Combine(directory, String.Format("background{0}.bmp", backgroundNumber));
+            }
+            backgroundImage.Save(fullBackgroundFilePath, System.Drawing.Imaging.ImageFormat.Bmp);
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            switch (style)
+            {
+                case PicturePosition.Tile:
+                    key.SetValue(@"PicturePosition", "0");
+                    key.SetValue(@"TileWallpaper", "1");
+                    break;
+                case PicturePosition.Center:
+                    key.SetValue(@"PicturePosition", "0");
+                    key.SetValue(@"TileWallpaper", "0");
+                    break;
+                case PicturePosition.Stretch:
+                    key.SetValue(@"PicturePosition", "2");
+                    key.SetValue(@"TileWallpaper", "0");
+                    break;
+                case PicturePosition.Fit:
+                    key.SetValue(@"PicturePosition", "6");
+                    key.SetValue(@"TileWallpaper", "0");
+                    break;
+                case PicturePosition.Fill:
+                    key.SetValue(@"PicturePosition", "10");
+                    key.SetValue(@"TileWallpaper", "0");
+                    break;
+            }
+            key.Close();
+            SystemParametersInfo(SET_DESKTOP_BACKGROUND, 0, fullBackgroundFilePath, UPDATE_INI_FILE | SEND_WINDOWS_INI_CHANGE);
+        }
+    }
+}
