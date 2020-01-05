8e0f914cf264155d5a1e12d9de09b125b6d98053
Sanket Mehrotra <sanketmehrotra102@gmail.com>
2020-01-04 19:09:04 -0700

    Add files via upload

---
+++ calendar_excel_desktop/calendar_excel_desktop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace calendar_excel_desktop
+{
+    class Excel_reader
+    {
+        private Excel_reader()
+        {
+             Excel.Workbook MyBook = null;
+             Excel.Worksheet MySheet = null;
+             Excel.Application MyApp = null;
+
+            string cal_path = Environment.SpecialFolder.MyDocuments.ToString();
+
+            MyApp = new Excel.Application();
+            MyApp.Visible = false;
+            MyBook = MyApp.Workbooks.Open(cal_path + "2018 Calendar.xlsx");
+            MySheet = MyBook.Sheets[1];
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
